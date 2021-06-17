6ebb02c550cbadca63e4d04c58a7318f611b71ba
Gabriel Victoria <81654814+gabrielvictoria96@users.noreply.github.com>
2021-06-17 01:40:43 -0300

    menu slide
    menu slide acrescentado e alterações no javascript

--- script/script.js
+++ script/script.js
@@ -4,6 +4,10 @@ const container = document.querySelector(".container")
 const menuSection = document.querySelector('.menu_section')
 const hamburger = document.querySelector('.hamburger');
 const navLink = document.querySelector("#nav_ul")
+const coffee = document.getElementById("coffee")
+const donuts = document.getElementById("donuts")
+const menuItemDonut = document.querySelector(".menu_items.donut")
+const menuItemCoffee = document.querySelector(".menu_items.coffee")
 
 
 hamburger.addEventListener("click", () => {
@@ -22,3 +26,19 @@ navLink.addEventListener("click", () => {
     
     document.body.style.overflow = "initial"
 });
+
+donuts.addEventListener("click", () => {
+
+    menuItemDonut.classList.add("active");
+    menuItemCoffee.classList.remove("active");
+    donuts.classList.add("active");
+    coffee.classList.remove("active");
+});
+
+coffee.addEventListener("click", () =>{
+
+    menuItemDonut.classList.remove("active");
+    menuItemCoffee.classList.add("active");
+    donuts.classList.remove("active");
+    coffee.classList.add("active");
+})
